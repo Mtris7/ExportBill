c15d3b750972b0d8f27c1f2c724635c40fa143e1
Minh Tri <minhtri.win@gmail.com>
2021-06-29 11:02:20 +0700

    update settings debug or release

--- ExportBill/Settings.cs
+++ ExportBill/Settings.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ExportBill
+﻿namespace ExportBill
 {
     public class Settings
     {
+#if DEBUG
         public const string API = "api";
         public const string userName = "apitest@tienthu.vn";
         public const string passWord = "62&z!]r*RV";
-
-        //public const string API = "app";
-        //public const string userName = "tienthuapi@tienthu.vn";
-        //public const string passWord = "Qq>a$5rWTf";
+#else
+                public const string API = "app";
+                public const string userName = "tienthuapi@tienthu.vn";
+                public const string passWord = "Qq>a$5rWTf";  
+#endif
 
         public static string token = string.Empty;
         public const string URL = @"http://" + Settings.API + ".ototienthu.com.vn/api/v1/oauth/token";
